8a9ec734593567d0976255d6e9acf7ab2d3cd3cb
Noor Sheikh <noorsheikh.me@gmail.com>
2024-12-08 23:16:23 -0500

    wireup listing all google drive files api to UI

---
+++ client/src/core/googleDrive/models/File/index.ts
@@ -0,0 +1,8 @@
+export interface File {
+  title: string;
+  thumbnailLink: string;
+  iconLink: string;
+  createdData: string;
+  modifiedDate: string;
+  fileExtension: string;
+}
---
+++ client/src/core/googleDrive/services/getAllFiles/index.ts
@@ -0,0 +1,16 @@
+import { serverBaseUrl } from "@/config";
+import { File } from "../../models/File";
+
+const getAllFiles = async (accessToken: string): Promise<File | undefined> => {
+  try {
+    const response = await fetch(
+      `${serverBaseUrl}/allfiles?access_token=${accessToken}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching current user info, ERROR: ", error);
+    return;
+  }
+};
+
+export default getAllFiles;
