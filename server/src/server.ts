ea8f1851fbef8be5b3ace069ed37c9bde1ccb536
Noor Sheikh <noorsheikh.me@gmail.com>
2024-12-10 00:41:20 -0500

    setup /upload-file api

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import { google } from "googleapis";
 import cors from "cors";
 import fs from "fs";
+import multer from "multer";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +13,8 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+const upload = multer({ dest: "uploads/" });
 
 const port = 3000;
 
@@ -114,6 +117,74 @@ app.get("/remove-file", async (req: Request, res: Response) => {
     res.status(400).send(error);
   }
 });
+
+app.post(
+  "/upload-file",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    if (!req?.file) {
+      res.status(400).send("No file provided.");
+    }
+
+    const { access_token = "" } = req?.query;
+
+    const oauth2Client = new google.auth.OAuth2();
+    if (access_token && typeof access_token === "string") {
+      oauth2Client.setCredentials({
+        access_token,
+      });
+    } else {
+      res.status(400).send("Invalid access token parameter provided.");
+    }
+
+    try {
+      if (req?.file?.path) {
+        const drive = google.drive({ version: "v2", auth: oauth2Client });
+        const response = await drive.files.insert({
+          media: {
+            mimeType: req?.file?.mimetype,
+            body: fs.createReadStream(req.file.path),
+          },
+          requestBody: {
+            title: req?.file?.originalname,
+          },
+        });
+        fs.unlink(req.file.path, (error) => {
+          console.error("Error cleaning up file, ERROR: ", error);
+        });
+        const {
+          id,
+          title,
+          thumbnailLink,
+          iconLink,
+          fileExtension,
+          createdDate,
+          modifiedDate,
+          webContentLink,
+          exportLinks,
+          alternateLink,
+        } = response?.data;
+        res.status(200).json({
+          id,
+          title,
+          thumbnailLink,
+          iconLink,
+          fileExtension,
+          createdDate,
+          modifiedDate,
+          webContentLink,
+          exportLinks,
+          alternateLink,
+        });
+      } else {
+        res.status(400).send("File content not found.");
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server address: http://localhost:${port}.`);
